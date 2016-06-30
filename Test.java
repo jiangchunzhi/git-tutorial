be2eef709452ad3d2b47cecc1246a9d29557950d
jiangchunzhi <jiangchunzhi_2007@126.com>
2016-06-30 15:07:18 +0800

    add test java

---
+++ Test.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println(new Date(1467183540000L));
+		System.out.println(21005 % 64);
+	}
+
+}
